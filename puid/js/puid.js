0996fd63e636024918345ebbfee1568fb56c0953
Domachin <gio-_-@hotmail.it>
2020-03-01 23:13:16 +0100

    Update puid.js

--- puid/js/puid.js
+++ puid/js/puid.js
@@ -61,7 +61,41 @@ $(function()
 	});
 			   
 	window.alert(selection.length);
-	$("#SetID").on("click", SetID(selection));
+	$("#SetID").on("click", function() {
+   window.alert("start function");
+   RM.Data.getAttributes(selection, function(result){
+      
+      // Store any required attribute changes here
+      var toSave = [];
+      window.alert("get attributes");
+       // Go through artifact data examining artifact type
+      result.data.forEach(function(item){
+         var type = item.values[RM.Data.Attributes.ARTIFACT_TYPE];
+         window.alert(type);
+         var newid = "";
+	 var n = -1;
+	 for (var i = 0; i < 8; i++)
+	 {
+		 if(identified_artifacts[i].includes(type)) n=i;
+	 }
+	// if(n!=-1)
+	 //{
+	 //   newid = prefixes[i]+counters[i];
+	 //   counters[i]++;
+         //   item.values[identifiers[i]] = newid;
+         //   toSave.push(item);
+	 //}
+      });
+      
+      // Perform a bulk save for all changed attributes
+      RM.Data.setAttributes(toSave, function(result){
+         if(result.code !== RM.OperationResult.OPERATION_OK)
+         {
+            window.alert("Error: " + code);
+         }
+      });
+   });
+});
 });
 
 
