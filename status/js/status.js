3cf58d95cdce3d7ccbfb74609ff1481a38447766
Domachin <gio-_-@hotmail.it>
2020-03-03 17:00:02 +0100

    Update status.js

--- status/js/status.js
+++ status/js/status.js
@@ -1,6 +1,3 @@
-var identified_artifacts = ["Condizione applicativa","Contromisura","Hazard","Requisito cliente","Requisito sistema","Requisito software","Requisito sottosistema","Test"];
-var identifiers = ["Identificativo Condizione Applicativa","Identificativo Contromisura","Identificativo Hazard","Identificativo UN","Identificativo ERIS","Identificativo Software","Identificativo Sottosistema","Identificativo Test"];
-var prefixes = ["xxx_","xxx_","xxx_","xxx_","xxx_","xxx_","xxx_","xxx_"];
 var initialize = true;
 
 function updateCounters()
@@ -11,18 +8,18 @@ function updateCounters()
 
 $(function()
 {
-	if (initialize==true) updateCounters();
+	//if (initialize==true) updateCounters();
 	
 	RM.Event.subscribe(RM.Event.ARTIFACT_SAVED, function(selected) {
-		window.alert("saved");
+		//window.alert("saved");
 		RM.Data.getAttributes(selection, identifiers.concat([RM.Data.Attributes.ARTIFACT_TYPE]), function(result){
 			var toSave = [];
 			var item = result.data[0];
 			var type = item.values[RM.Data.Attributes.ARTIFACT_TYPE].name;
 			var status = item.values["State (Workflow "+type+")"];
 			var allocation = item.values["Allocazione"];
-			window.alert(status+" "+allocation);
-			if(status=="Identificato" && allocation!=undefined && allocation!="") item.values["State (Workflow "+type+")"]="Caratterizzato";
+			//window.alert(status+" "+allocation);
+			if(status=="Caratterizzato" && allocation!=undefined && allocation!="") item.values["State (Workflow "+type+")"]="Allocato";
 			toSave.push(item);
 			RM.Data.setAttributes(toSave, function(result2){
          			if(result2.code !== RM.OperationResult.OPERATION_OK)
