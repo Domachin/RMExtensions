cf56265d4fc35ab72e041417f252c6d924aa13f8
Domachin <gio-_-@hotmail.it>
2020-03-03 10:05:51 +0100

    Update id.js

--- id/js/id.js
+++ id/js/id.js
@@ -2,17 +2,17 @@ var identified_artifacts = ["Condizione applicativa","Contromisura","Hazard","Re
 var identifiers = ["Identificativo Condizione Applicativa","Identificativo Contromisura","Identificativo Hazard","Identificativo UN","Identificativo ERIS","Identificativo Software","Identificativo Sottosistema","Identificativo Test"];
 var prefixes = ["xxx_","xxx_","xxx_","xxx_","xxx_","xxx_","xxx_","xxx_"];
 var initialize = true;
-var counters = [1,1,1,1,1,1,1,1];
+var counters = [0,0,0,0,0,0,0,0];
 
-function updateCounters()
+function version()
 {
-	window.alert("initialization 43");
+	window.alert("43");
 	initialize=false;
 }
 
 $(function()
 {
-	if (initialize==true) updateCounters();
+	//if (initialize==true) updateCounters();
 	
 	var selection = [];
 	RM.Event.subscribe(RM.Event.ARTIFACT_OPENED, function(selected) {
@@ -21,13 +21,13 @@ $(function()
 			result3.data.forEach(function(item2){
 				for(var i = 0; i < 8;i++)
 				{
-			 		window.alert("counter ["+i+"]="+counters[i]+":"+identifiers[i]);
+			 		//window.alert("counter ["+i+"]="+counters[i]+":"+identifiers[i]);
 				 	var oldid = item2.values[identifiers[i]];
 					var num = 0;
-					window.alert(oldid+" "+prefixes[i]);
+					//window.alert(oldid+" "+prefixes[i]);
 					if (oldid==undefined) oldid="";
 					if(oldid.includes(prefixes[i])) num=parseInt(oldid.slice(-3));
-					window.alert("counter "+counters[i]+" num "+num);
+					//window.alert("counter "+counters[i]+" num "+num);
 					if (num>counters[i]) counters[i]=num;
 					if(isNaN(num)) window.alert("Number error");
 				}
@@ -38,36 +38,39 @@ $(function()
   $("#SetID").on("click", function() {
 	  
       
-      window.alert("start function");
+      //window.alert("start function");
 	  
       RM.Data.getContentsAttributes(selection, identifiers.concat([RM.Data.Attributes.ARTIFACT_TYPE]), function(result){
 	      
       if(result.code !== RM.OperationResult.OPERATION_OK)
       {
+	 window.alert("Error: " + result.code);
          return;
       }
       
       // Store any required attribute changes here
       var toSave = [];
-      window.alert("get attributes");
+      //window.alert("get attributes");
+	      
        // Go through artifact data examining artifact type
       result.data.forEach(function(item){
          var type = item.values[RM.Data.Attributes.ARTIFACT_TYPE].name;
-         window.alert(type);
+         //window.alert(type);
          var newid = "";
 	 var n = -1;
 	 for (var i = 0; i < 8; i++)
 	 {
 		 //window.alert(identified_artifacts[i]+" vs "+type);
-		 if(identified_artifacts[i].includes(type)) {n=i; window.alert("found: "+i);}
+		 if(identified_artifacts[i].includes(type)) n=i; //window.alert("found: "+i);
 	 }
 	 if(n!=-1)
 	 {
 		 
             if (item.values[identifiers[n]]==null || !(item.values[identifiers[n]].includes(prefixes[n])))
 	    {
-		newid = prefixes[n]+('000'+counters[n]).slice(-3);
-	    	window.alert(newid);
+		var counter = counters[n]+1;
+		newid = prefixes[n]+('000'+counter).slice(-3);
+	    	//window.alert(newid);
 	    	item.values[identifiers[n]] = newid;
 		counters[n]++;
             	toSave.push(item);
@@ -79,7 +82,7 @@ $(function()
       RM.Data.setAttributes(toSave, function(result2){
          if(result2.code !== RM.OperationResult.OPERATION_OK)
          {
-            window.alert("Error: " + code);
+            window.alert("Error: " + result2.code);
          }
       });
    });
