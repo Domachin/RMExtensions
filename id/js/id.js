f6ea5dc827a9e9a2c43265388e8ddf9827045ff0
Domachin <gio-_-@hotmail.it>
2020-04-08 21:05:32 +0200

    Update id.js

--- id/js/id.js
+++ id/js/id.js
@@ -6,7 +6,7 @@ var counters = [0,0,0,0,0,0,0,0,0];
 
 function version()
 {
-	window.alert("45");
+	window.alert("46");
 	initialize=false;
 }
 
@@ -16,7 +16,7 @@ $(function()
 	
 	var selection = [];
 	var docName = "";
-	$("#log").append("Prima di aggiornare gli identificativi occorre entrare in un modulo");
+	$("#log").html("<p class='info'>Prima di aggiornare gli identificativi occorre entrare in un modulo</p>");
 	RM.Event.subscribe(RM.Event.ARTIFACT_OPENED, function(selected) {
 		selection = selected;
 		RM.Data.getContentsAttributes(selection, identifiers, function(result3){
@@ -36,7 +36,7 @@ $(function()
 			});
 		});
 		$("#log").empty();
-		$("#log").append("Modulo: "+item3.values[RM.Data.Attributes.NAME]);
+		$("#log").html("<p class='info'>Modulo: "+item3.values[RM.Data.Attributes.NAME]+"</p>");
 		RM.Data.getAttributes(selection, [RM.Data.Attributes.NAME], function(result4){
 			result4.data.forEach(function(item3){
 				docName=item3.values[RM.Data.Attributes.NAME]+"_";
