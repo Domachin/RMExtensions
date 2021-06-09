9cc0ea38b35a4d334c47e1c97c41d9a0c8d92fcb
Domachin <gio-_-@hotmail.it>
2021-06-09 16:31:08 +0200

    Update id.js

--- id/js/id.js
+++ id/js/id.js
@@ -56,7 +56,7 @@ $(function()
 				if (item4.values[RM.Data.Attributes.FORMAT] === RM.Data.Formats.MODULE)
 				{
 					$("#intro").empty();
-					println("Modulo: <b>"+item4.values[RM.Data.Attributes.NAME]+"</b><br/><br/>Se si effettuano modifiche, uscire e rientrare nel modulo prima di ricalcolare.","intro");
+					println("Modulo: <b>"+item4.values[RM.Data.Attributes.NAME]+"</b><br/><br/><small>Se si effettuano modifiche, uscire e rientrare nel modulo prima di ricalcolare.</small>","intro");
 					docName=item4.values[RM.Data.Attributes.NAME]+"_";
 				}
 			});
